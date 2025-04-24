2f04ae27ef048e3562fe56d2914f8b368f9e8880
Athul Shaju <athul.shaju4@gmail.com>
2025-04-24 21:25:06 +0530

    Creating a express todoapp.Added routes and tests for api

---
+++ todo-app/__tests__/todo.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+
+const request=require("supertest");
+
+const db=require("../models/index");
+const app=require("../app");
+
+let server,agent;
+
+describe("Todo Test Suite", () => {
+    beforeAll(async () => {
+        await db.sequelize.sync({ force: true });
+        server = app.listen(3000, () => { })
+        agent=request.agent(server);
+    })
+
+    afterAll(async () => {
+        await db.sequelize.close();
+        server.close();
+    })
+
+    test("responds with json at /todos", async () => {
+        const response = await agent.post("/todos").send({
+            "title" :"Buy milk",
+            "dueDate": new Date().toISOString(),
+            "completed": false
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.header['content-type']).toBe("application/json; charset=utf-8");
+        const parsedResponse = JSON.parse(response.text);
+        expect(parsedResponse.id).toBeDefined();
+    })
+
+    test("test markAsCompleted", async () => {
+        const response = await agent.post("/todos").send({
+            "title" :"Buy milk",
+            "dueDate": new Date().toISOString(),
+            "completed": false
+        });
+        const parsedResponse = JSON.parse(response.text);
+        const todoId = parsedResponse.id;
+        expect(parsedResponse.completed).toBe(false);
+
+        const markAsCompletedResponse = await agent.put(`/todos/${todoId}/markAsCompleted`).send();
+        const parsedUpdateResponse = JSON.parse(markAsCompletedResponse.text);
+        expect(parsedUpdateResponse.completed).toBe(true);
+    });
+})
---
+++ todo-app/app.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const todo = require("../todo-cli/models/todo");
+const app=express();
+const {Todo} = require("./models")
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/todos",async(request,response)=>{
+    //response.send("Hello World");
+    console.log("Todo list");
+    const todoItem=await todo.getAllTodos();
+    response.json(todoItem);
+    
+
+})
+
+
+app.post("/todos",async (request,response)=>{
+    console.log("Creating Todo",request.body);
+    try{
+        const todo=await Todo.addTodo({title: request.body.title,dueDate: request.body.dueDate,completed: false})
+        return response.json(todo);
+    }
+    catch(error){
+        console.log(error);
+        return response.status(422).json(error);
+    }
+    
+
+})
+
+app.put("/todos/:id/markAsCompleted", async (request,response)=>{
+    console.log("todo updated",request.params.id);
+    const todo=await Todo.findByPk(request.params.id)
+    try{
+        
+        const updatedTodo = await todo.markAsCompleted()
+        return response.json(updatedTodo);
+    }
+    catch(error){
+        console.log(error);
+        return response.status(422).json(error);
+    }
+})
+
+app.delete("/todos/:id",(request,response)=>{
+    console.log("todo deleted",request.params.id);
+})
+
+module.exports=app; 
