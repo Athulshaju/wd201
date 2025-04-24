8f516dace921829d2f000f4ff248e495d092c8b0
Athul Shaju <athul.shaju4@gmail.com>
2025-04-25 00:17:29 +0530

    added html pages and ready for prod in render

--- todo-app/models/index.js
+++ todo-app/models/index.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-undef */
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+// eslint-disable-next-line no-undef
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
